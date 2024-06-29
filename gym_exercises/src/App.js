b0cd929c297cd6ae3788656eb46fbe28358e376f
gokull-1998 <gokulakrishnanm1998@gmail.com>
2024-06-29 19:20:38 +0530

    added till 15

--- gym_exercises/src/App.js
+++ gym_exercises/src/App.js
@@ -1,12 +1,25 @@
 import React from 'react'
 
+import {Route,Routes} from 'react-router-dom'
+import {Box} from '@mui/material';
+
 // rafce - es7+ react snippets
+import ExerciseDetail from './pages/ExerciseDetail';
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import './App.css';
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <Box width="400px" sx={{width:{x1:'1488px'}}}>
+        <Navbar/>
+        <Routes>
+            <Route path='/' element={<Home/>}/>
+            <Route path='/exercise/:id' element={<ExerciseDetail/>}/>
+        </Routes>
+        <Footer/>
+    </Box>
   )
 }
 
