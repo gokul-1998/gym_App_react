695fe40e92d8d0fd39f046090d2aa654cf236fca
gokull-1998 <gokulakrishnanm1998@gmail.com>
2024-06-30 19:02:33 +0530

    1.55 - bug image

--- gym_exercises/src/pages/ExerciseDetail.js
+++ gym_exercises/src/pages/ExerciseDetail.js
@@ -1,10 +1,37 @@
-import React from 'react'
+import React,{useEffect,useState} from 'react'
+import { useParams } from 'react-router-dom'
+import {Box,Typography,Button} from '@mui/material'
+
+import { exerciseOptions,fetchData } from '../utils/fetchData'
+
+import Detail from '../components/Detail'
+import ExerciseVideos from '../components/ExerciseVideos'
+import SimilarExercises from '../components/SimilarExercises'
 
 const ExerciseDetail = () => {
+  const [exerciseDetail,setExerciseDetail]=useState({})
+  const {id}=useParams()
+
+
+  useEffect(()=>{
+    const fetchExerciseDetail=async()=>{
+      const exerciseDbUrl='https://exercisedb.p.rapidapi.com'
+      const youtubeSearchUrl='https://youtube-search-and-download.p.rapidapi.com';
+
+      const exerciseDetailData=await fetchData(`${exerciseDbUrl}/exercises/exercise/${id}`,exerciseOptions);
+
+      setExerciseDetail(exerciseDetailData)
+    }
+    fetchExerciseDetail();
+  },[id])
+
   return (
-    <div>
-      ExerciseDetails
-    </div>
+    <Box>
+      <Detail exerciseDetail={exerciseDetail}/>
+      <ExerciseVideos/>
+      <SimilarExercises/>
+
+    </Box>
   )
 }
 
